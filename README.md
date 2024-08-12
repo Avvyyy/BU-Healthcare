1866a1229e6856bce2db9c1b618a16bf02fd9337
Favour A. Uzochukwu <uzochukwu.favour310@gmail.com>
2024-08-12 07:11:42 +0100

    Complete installation of vue-fire

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # BU-Healthcare
 Emergency healthcare system built in NuxtJS, Laravel and Firebase
+
+## Tools used
+- Vuelidate - For form validation
+- Vuefire - Firebase integration
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,15 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "nuxt-vuefire"],
+ vuefire: {
+  config: {
+    // Config from firebase project settings
+    apiKey: "AIzaSyD3zbPsXZVFp63mmANX01VGWPGrDLdejrs",
+    authDomain: "bu-healthcare.firebaseapp.com",
+    projectId: "bu-healthcare",
+    storageBucket: "bu-healthcare.appspot.com",
+    messagingSenderId: "714665042795",
+    appId: "1:714665042795:web:00568c211687e0b8cee22e"
+  }
+ }
 })
